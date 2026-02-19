99ffae07a1ac36efab43930c703aa9f9b058bdbf
chirag100x <chiragchoudhary9077@gamil.com>
2026-02-20 02:17:10 +0530

    something

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3080/api/:path*",
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
       },
     ];
   },
